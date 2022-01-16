25d5c153bad25ce05aa2edc50efc6744759dde48
Netanel Gilad <netanelg@wix.com>
2022-01-16 19:32:00 +0000

    parseJSON should return unknown

--- src/host.ts
+++ src/host.ts
@@ -1,6 +1,6 @@
 export const logToConsole = console.log;
 export const stdout = process.stdout;
 export const stderr = process.stderr;
-export const parseJSON = JSON.parse;
+export const parseJSON = JSON.parse as (text: string, reviver?: ((this: any, key: string, value: any) => any) | undefined) => unknown;
 export { forkProgram } from './forkProgram';
 export { executeProgram } from './executeProgram';
